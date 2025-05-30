38f0adf6c0a1723c7dda0406cf3f0e8cb1d531c2
Anshul Joshi <anshuljoshi21@gmail.com>
2025-05-30 21:31:33 +0530

    updates

--- c_files/raylib/dungeon_delve_text_rpg/test.c
+++ c_files/raylib/dungeon_delve_text_rpg/test.c
@@ -1,64 +1,74 @@
 #include <raylib.h>
 
-#define SIZE 10
-
-void
-center_and_draw_array(char* arr[],
-					  int font_size,
-					  int line_gap,
-					  Rectangle rect,
-					  int selected)
-{
-	int total_text_height = SIZE * font_size;
-	int start_y = rect.y + (rect.height - total_text_height) / 2;
-
-	for (int i = 0; i < SIZE; i++) {
-		int text_width = MeasureText(arr[i], font_size);
-		int text_x = (int)rect.x + (int)rect.width / 2 - text_width / 2;
-		int text_y = start_y + i * (font_size + line_gap);
-
-		Color color = (i == selected) ? BLACK : GRAY;
-
-		DrawText(arr[i], text_x, text_y, font_size, color);
-
-		if (i == selected) {
-			DrawText(">", text_x - 30, text_y - 5, 40, BLACK);
-		}
-	}
-}
-
 int
 main(void)
 {
 	const int SCREEN_WIDTH = 800;
 	const int SCREEN_HEIGHT = 600;
-	const char SCREEN_TITLE[] = "Auto centering test";
-	const Color SCREEN_BACKGROUND = LIGHTGRAY;
+	const char SCREEN_TITLE[] = "";
+	const Color SCREEN_BACKGROUND = RAYWHITE;
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
 
-	Rectangle rect = (Rectangle){ 0, 0, GetScreenWidth(), GetScreenHeight() };
-	char* arr[SIZE] = { "TEST1", "TEST2", "TEST3", "TEST4", "TEST5",
-						"TEST6", "TEST7", "TEST8", "TEST9", "TEST10" };
-	int font_size = 30;
+	char* arr[] = { "test1",  "test2",	"test3",  "test4",	"test5",
+					"test6",  "test7",	"test8",  "test9",	"test10",
+					"test11", "test12", "test13", "test14", "test15",
+					"test16", "test17", "test18", "test19", "test20" };
+
+	int total_elements = sizeof(arr) / sizeof(arr[0]);
+
+	Rectangle layout_rect =
+	  (Rectangle){ 0, 0, GetScreenWidth(), GetScreenHeight() };
+
 	int selected = 0;
+	int first_visible_element = 0;
+	int font_size = 40; // text_height
 	int line_gap = 5;
 	int line_height = font_size + line_gap;
-	int max_visible = rect.height / line_height;
-	int scroll_offset = 0;
+	int max_visible_elements = (layout_rect.height - line_gap) / line_height;
+	int visible_text_height = (font_size * max_visible_elements) +
+							  (line_gap * (max_visible_elements - 1));
 
 	while (!WindowShouldClose()) {
-		if (IsKeyPressed(KEY_DOWN)) {
-			selected = (selected + 1) % SIZE;
-		}
+
 		if (IsKeyPressed(KEY_UP)) {
-			selected = (selected - 1 + SIZE) % SIZE;
+			selected--;
+			if (selected < 0) {
+				selected = 0;
+			}
+
+			if (selected < first_visible_element) {
+				first_visible_element = selected;
+			}
+		}
+		if (IsKeyPressed(KEY_DOWN)) {
+			selected++;
+			if (selected > total_elements - 1) {
+				selected = total_elements - 1;
+			}
+
+			if (selected > first_visible_element + max_visible_elements - 1) {
+				first_visible_element++;
+			}
 		}
 
 		BeginDrawing();
 		ClearBackground(SCREEN_BACKGROUND);
 
-		center_and_draw_array(arr, font_size, line_gap, rect, selected);
+		for (int i = first_visible_element; i < total_elements; i++) {
+			int text_width = MeasureText(arr[i], font_size);
+			int text_x =
+			  (int)layout_rect.x + (int)layout_rect.width / 2 - text_width / 2;
+			int text_y = (int)layout_rect.y + (int)layout_rect.height / 2 -
+						 visible_text_height / 2 + ((i - first_visible_element) * line_height);
+
+			Color color = (i == selected) ? BLACK : GRAY;
+			DrawText(arr[i], text_x, text_y, font_size, color);
+
+			if (i == selected) {
+				DrawText(">", text_x - 30, text_y, font_size, color);
+			}
+		}
 
 		EndDrawing();
 	}
