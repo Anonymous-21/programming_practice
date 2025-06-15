03d59182603c902421e07fff5dec4c281151cd39
Anshul Joshi <anshuljoshi21@gmail.com>
2025-06-15 18:16:10 +0530

    chess update

--- c_files/raylib/chess/chess.c
+++ c_files/raylib/chess/chess.c
@@ -2,369 +2,207 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 800
+#define SCREEN_SIZE 800
 #define SCREEN_TITLE "CHESS"
-#define SCREEN_BACKGROUND LIGHTGRAY
+#define SCREEN_BACKGROUND RAYWHITE
 #define GAME_FPS 60
 
-#define ROWS 8
-#define COLS 8
-#define SLOT_WIDTH (SCREEN_WIDTH / ROWS)
-#define SLOT_HEIGHT (SCREEN_HEIGHT / COLS)
+#define BOARD_SIZE 8
+#define TILE_SIZE (SCREEN_SIZE / BOARD_SIZE)
+
+#define PIECE_COUNT 12
+#define ASCII_SIZE 128
 
 void
-draw_board()
+texture_arr_init(Texture2D* texture_arr)
 {
-    for (int i = 0; i < ROWS; i++)
+    texture_arr[0] = LoadTexture("./assets/b_king_1x_ns.png");
+    texture_arr[1] = LoadTexture("./assets/b_queen_1x_ns.png");
+    texture_arr[2] = LoadTexture("./assets/b_rook_1x_ns.png");
+    texture_arr[3] = LoadTexture("./assets/b_knight_1x_ns.png");
+    texture_arr[4] = LoadTexture("./assets/b_bishop_1x_ns.png");
+    texture_arr[5] = LoadTexture("./assets/b_pawn_1x_ns.png");
+    texture_arr[6] = LoadTexture("./assets/w_king_1x_ns.png");
+    texture_arr[7] = LoadTexture("./assets/w_queen_1x_ns.png");
+    texture_arr[8] = LoadTexture("./assets/w_rook_1x_ns.png");
+    texture_arr[9] = LoadTexture("./assets/w_knight_1x_ns.png");
+    texture_arr[10] = LoadTexture("./assets/w_bishop_1x_ns.png");
+    texture_arr[11] = LoadTexture("./assets/w_pawn_1x_ns.png");
+}
+
+void
+piece_index_map_init(int* piece_index_map)
+{
+    // small_case is black
+    piece_index_map['k'] = 0;
+    piece_index_map['q'] = 1;
+    piece_index_map['r'] = 2;
+    piece_index_map['n'] = 3;
+    piece_index_map['b'] = 4;
+    piece_index_map['p'] = 5;
+
+    // upper_case is white
+    piece_index_map['K'] = 6;
+    piece_index_map['Q'] = 7;
+    piece_index_map['R'] = 8;
+    piece_index_map['N'] = 9;
+    piece_index_map['B'] = 10;
+    piece_index_map['P'] = 11;
+}
+
+void
+draw_board(char board[BOARD_SIZE][BOARD_SIZE],
+           Texture2D* texture_arr,
+           int* piece_index_map,
+           int piece_padding)
+{
+    for (int row = 0; row < BOARD_SIZE; row++)
     {
-        for (int j = 0; j < COLS; j++)
+        for (int col = 0; col < BOARD_SIZE; col++)
         {
-            int x = j * SLOT_WIDTH;
-            int y = i * SLOT_HEIGHT;
+            // row and col is board value from (0, 0) to (7, 7)
 
-            Color color = (i + j) % 2 == 0 ? LIGHTGRAY : DARKGRAY;
+            // draw background tiles
+            Color color = (row + col) % 2 == 0 ? LIGHTGRAY : DARKGRAY;
+            DrawRectangle(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE, color);
 
-            DrawRectangle(x, y, SLOT_WIDTH, SLOT_HEIGHT, color);
+            // draw pieces on tiles
+            char current_piece = board[row][col];
+            if (current_piece != '_')
+            {
+                int index = piece_index_map[( int ) current_piece];
+
+                Texture2D current_texture = texture_arr[index];
+                Rectangle source =
+                  (Rectangle){ 0, 0, current_texture.width, current_texture.height };
+                Rectangle dest = (Rectangle){ col * TILE_SIZE + piece_padding,
+                                              row * TILE_SIZE + piece_padding,
+                                              TILE_SIZE - piece_padding * 2,
+                                              TILE_SIZE - piece_padding * 2 };
+                Vector2 origin = (Vector2){ 0, 0 };
+                float rotation = 0.0f;
+                Color tint = WHITE;
+
+                DrawTexturePro(current_texture, source, dest, origin, rotation, tint);
+            }
         }
     }
 }
 
-typedef enum PieceType
-{
-    TYPE_KING,
-    TYPE_QUEEN,
-    TYPE_ROOK,
-    TYPE_KNIGHT,
-    TYPE_BISHOP,
-    TYPE_PAWN,
-
-} PieceType;
-
-typedef enum PieceColor
-{
-    COLOR_BLACK,
-    COLOR_WHITE,
-
-} PieceColor;
-
 void
-load_texture(Texture2D* texture, PieceType type, PieceColor color)
+drag_and_drop(char board[BOARD_SIZE][BOARD_SIZE],
+              Texture2D* texture_arr,
+              int* piece_index_map,
+              int piece_padding)
 {
-    switch (type)
+    static bool is_dragging = false;
+    static int start_row = -1;
+    static int start_col = -1;
+    static char current_piece = '_';
+    static Vector2 current_piece_offset = (Vector2){ 0, 0 };
+    static Vector2 current_piece_pos;
+
+    Vector2 mouse_pos = GetMousePosition();
+    // row and col is board value from (0, 0) to (7, 7)
+    int row = ( int ) (mouse_pos.y / TILE_SIZE);
+    int col = ( int ) (mouse_pos.x / TILE_SIZE);
+
+    current_piece_pos =
+      (Vector2){ col * TILE_SIZE + piece_padding, row * TILE_SIZE + piece_padding };
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !is_dragging)
     {
-        case TYPE_KING:
-            if (color == COLOR_BLACK)
-            {
-                *texture = LoadTexture("assets/b_king_1x_ns.png");
-            }
-            else if (color == COLOR_WHITE)
-            {
-                *texture = LoadTexture("assets/w_king_1x_ns.png");
-            }
-            break;
-        case TYPE_QUEEN:
-            if (color == COLOR_BLACK)
-            {
-                *texture = LoadTexture("assets/b_queen_1x_ns.png");
-            }
-            else if (color == COLOR_WHITE)
-            {
-                *texture = LoadTexture("assets/w_queen_1x_ns.png");
-            }
-            break;
-        case TYPE_ROOK:
-            if (color == COLOR_BLACK)
-            {
-                *texture = LoadTexture("assets/b_rook_1x_ns.png");
-            }
-            else if (color == COLOR_WHITE)
-            {
-                *texture = LoadTexture("assets/w_rook_1x_ns.png");
-            }
-            break;
-        case TYPE_KNIGHT:
-            if (color == COLOR_BLACK)
-            {
-                *texture = LoadTexture("assets/b_knight_1x_ns.png");
-            }
-            else if (color == COLOR_WHITE)
-            {
-                *texture = LoadTexture("assets/w_knight_1x_ns.png");
-            }
-            break;
-        case TYPE_BISHOP:
-            if (color == COLOR_BLACK)
-            {
-                *texture = LoadTexture("assets/b_bishop_1x_ns.png");
-            }
-            else if (color == COLOR_WHITE)
-            {
-                *texture = LoadTexture("assets/w_bishop_1x_ns.png");
-            }
-            break;
-        case TYPE_PAWN:
-            if (color == COLOR_BLACK)
-            {
-                *texture = LoadTexture("assets/b_pawn_1x_ns.png");
-            }
-            else if (color == COLOR_WHITE)
-            {
-                *texture = LoadTexture("assets/w_pawn_1x_ns.png");
-            }
-            break;
-        default:
-            fprintf(stderr, "texture not found\n");
-            exit(EXIT_FAILURE);
-            break;
-    }
-}
+        if (board[row][col] != '_')
+        {
+            current_piece = board[row][col];
+            start_row = row;
+            start_col = col;
+            board[start_row][start_col] = '_';
+            is_dragging = true;
 
-typedef struct Piece
-{
-    PieceType type;
-    PieceColor color;
-    Texture2D texture;
-    Rectangle source_rect;
-    Rectangle dest_rect;
-    Vector2 origin;
-    float rotation;
-    Color tint;
-    bool active;
-    bool is_dragging;
-    Vector2 offset;
-
-} Piece;
-
-void
-piece_init(Piece* piece, PieceType type, PieceColor color, float x, float y, int padding)
-{
-    piece->type = type;
-    piece->color = color;
-    load_texture(&piece->texture, piece->type, piece->color);
-    piece->source_rect = (Rectangle){ 0, 0, piece->texture.width, piece->texture.height };
-    piece->dest_rect = (Rectangle){ x, y, SLOT_WIDTH - padding * 2, SLOT_HEIGHT - padding * 2 };
-    piece->origin = (Vector2){ 0, 0 };
-    piece->rotation = 0.0f;
-    piece->tint = WHITE;
-    piece->active = true;
-    piece->is_dragging = false;
-    piece->offset = (Vector2){ 0, 0 };
-}
-
-void
-piece_draw(Piece* piece)
-{
-    DrawTexturePro(piece->texture,
-                   piece->source_rect,
-                   piece->dest_rect,
-                   piece->origin,
-                   piece->rotation,
-                   piece->tint);
-}
-
-// PIECE LIST
-void
-pieces_init(Piece* pieces, int piece_padding)
-{
-    piece_init(&pieces[0],
-               TYPE_ROOK,
-               COLOR_BLACK,
-               0 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[1],
-               TYPE_KNIGHT,
-               COLOR_BLACK,
-               1 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[2],
-               TYPE_BISHOP,
-               COLOR_BLACK,
-               2 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[3],
-               TYPE_QUEEN,
-               COLOR_BLACK,
-               3 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[4],
-               TYPE_KING,
-               COLOR_BLACK,
-               4 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[5],
-               TYPE_BISHOP,
-               COLOR_BLACK,
-               5 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[6],
-               TYPE_KNIGHT,
-               COLOR_BLACK,
-               6 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[7],
-               TYPE_ROOK,
-               COLOR_BLACK,
-               7 * SLOT_WIDTH + piece_padding,
-               0 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-
-    for (int i = 8; i < 16; i++)
-    {
-        piece_init(&pieces[i],
-                   TYPE_PAWN,
-                   COLOR_BLACK,
-                   (i - 8) * SLOT_WIDTH + piece_padding,
-                   1 * SLOT_HEIGHT + piece_padding,
-                   piece_padding);
+            current_piece_offset.x = mouse_pos.x - current_piece_pos.x;
+            current_piece_offset.y = mouse_pos.y - current_piece_pos.y;
+        }
     }
 
-    piece_init(&pieces[16],
-               TYPE_ROOK,
-               COLOR_WHITE,
-               0 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[17],
-               TYPE_KNIGHT,
-               COLOR_WHITE,
-               1 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[18],
-               TYPE_BISHOP,
-               COLOR_WHITE,
-               2 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[19],
-               TYPE_QUEEN,
-               COLOR_WHITE,
-               3 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[20],
-               TYPE_KING,
-               COLOR_WHITE,
-               4 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[21],
-               TYPE_BISHOP,
-               COLOR_WHITE,
-               5 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[22],
-               TYPE_KNIGHT,
-               COLOR_WHITE,
-               6 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-    piece_init(&pieces[23],
-               TYPE_ROOK,
-               COLOR_WHITE,
-               7 * SLOT_WIDTH + piece_padding,
-               7 * SLOT_HEIGHT + piece_padding,
-               piece_padding);
-
-    for (int i = 24; i < 32; i++)
+    if (is_dragging)
     {
-        piece_init(&pieces[i],
-                   TYPE_PAWN,
-                   COLOR_WHITE,
-                   (i - 24) * SLOT_WIDTH + piece_padding,
-                   6 * SLOT_HEIGHT + piece_padding,
-                   piece_padding);
-    }
-}
+        current_piece_pos.x = mouse_pos.x - current_piece_offset.x;
+        current_piece_pos.y = mouse_pos.y - current_piece_offset.y;
 
-void gen_current_board_fen(Piece * pieces, int pieces_count, int piece_padding)
-{
-    char * fen_string;
+        // draw piece
+        int index = piece_index_map[( int ) current_piece];
+        Texture2D current_texture = texture_arr[index];
+
+        Rectangle source = (Rectangle){ 0, 0, current_texture.width, current_texture.height };
+        Rectangle dest = (Rectangle){ current_piece_pos.x,
+                                      current_piece_pos.y,
+                                      TILE_SIZE - piece_padding * 2,
+                                      TILE_SIZE - piece_padding * 2 };
+        Vector2 origin = (Vector2){ 0, 0 };
+        float rotation = 0.0f;
+        Color tint = WHITE;
+
+        DrawTexturePro(current_texture, source, dest, origin, rotation, tint);
+    }
+
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && is_dragging)
+    {
+        if (board[row][col] == '_')
+        {
+            is_dragging = false;
+            board[row][col] = current_piece;
+        }
+    }
 }
 
 int
 main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
+    InitWindow(SCREEN_SIZE, SCREEN_SIZE, SCREEN_TITLE);
     SetTargetFPS(GAME_FPS);
 
-    int pieces_count = 32;
-    Piece pieces[pieces_count];
     int piece_padding = 20;
 
-    pieces_init(pieces, piece_padding);
+    Texture2D texture_arr[PIECE_COUNT];
+    texture_arr_init(texture_arr);
+
+    int piece_index_map[ASCII_SIZE];
+    piece_index_map_init(piece_index_map);
+
+    char board[BOARD_SIZE][BOARD_SIZE] = {
+        { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' }, { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+        { '_', '_', '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_', '_', '_' },
+        { '_', '_', '_', '_', '_', '_', '_', '_' }, { '_', '_', '_', '_', '_', '_', '_', '_' },
+        { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' }, { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
+    };
 
     while (!WindowShouldClose())
     {
-        Vector2 mouse_pos = GetMousePosition();
-
-        // drag and drop piece
-        for (int i = 0; i < pieces_count; i++)
+        // current board check
+        system("clear");
+        for (int i = 0; i < BOARD_SIZE; i++)
         {
-            if (pieces[i].active)
+            for (int j = 0; j < BOARD_SIZE; j++)
             {
-                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) &&
-                    CheckCollisionPointRec(mouse_pos, pieces[i].dest_rect) &&
-                    !pieces[i].is_dragging)
-                {
-                    pieces[i].is_dragging = true;
-                    pieces[i].offset.x = mouse_pos.x - pieces[i].dest_rect.x;
-                    pieces[i].offset.y = mouse_pos.y - pieces[i].dest_rect.y;
-                }
-
-                if (pieces[i].is_dragging)
-                {
-                    pieces[i].dest_rect.x = mouse_pos.x - pieces[i].offset.x;
-                    pieces[i].dest_rect.y = mouse_pos.y - pieces[i].offset.y;
-                }
-
-                if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
-                {
-                    pieces[i].is_dragging = false;
-
-                    // snap piece to slot
-                    for (int j = 0; j < ROWS; j++)
-                    {
-                        for (int k = 0; k < COLS; k++)
-                        {
-                            int x = k * SLOT_WIDTH;
-                            int y = j * SLOT_HEIGHT;
-
-                            if (CheckCollisionRecs(pieces[i].dest_rect,
-                                                   (Rectangle){ x, y, SLOT_WIDTH, SLOT_HEIGHT }))
-                            {
-                                pieces[i].dest_rect.x = x + piece_padding;
-                                pieces[i].dest_rect.y = y + piece_padding;
-                            }
-                        }
-                    }
-                }
+                printf(" %c ", board[i][j]);
             }
+            printf("\n\n");
         }
 
         BeginDrawing();
         ClearBackground(SCREEN_BACKGROUND);
 
-        draw_board();
-
-        for (int i = 0; i < pieces_count; i++)
-        {
-            if (pieces[i].active)
-            {
-                piece_draw(&pieces[i]);
-            }
-        }
+        draw_board(board, texture_arr, piece_index_map, piece_padding);
+        drag_and_drop(board, texture_arr, piece_index_map, piece_padding);
 
         EndDrawing();
+    }
+
+    // unload textures
+    for (int i = 0; i < PIECE_COUNT; i++)
+    {
+        UnloadTexture(texture_arr[i]);
     }
 
     CloseWindow();
